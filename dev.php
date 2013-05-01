ba6373ae13f18da1effae86f7f886fad0165d0ba
Rohan Chambers <rohanchambers888@yahoo.com>
2013-05-01 21:43:24 +0100

    udpating dev section

--- dev.php
+++ dev.php
@@ -10,14 +10,14 @@ $ogtags = 'http://www.rohanchambers.net/img/global/fb_logo.png'
 		<div id="content" class="clearfix">
 			<section id="portfolio">
 				<h1>Projects</h1>
-				<p>Some bits of functionality I developed mainly with the use of JavaScript/jQuery.<br/>
+				<p>Some bits of functionality I developed whilst learning JavaScript/jQuery.<br/> Currently delving into OOP JavaScript.
 				All code written from scratch.</p>
 				<ul class="bullets">
+					<li>Timeline pin animation: (First time using objects) <a href="js-projects/2012timeline/" target="_blank">View app</a> (March 10, 2012)</li>
+					<li>Infinite Carousel: <a href="js-projects/carousel-infinite/" target="_blank">View app</a> (November 10, 2011)</li>
 					<li>Tooltip constrainted: <a href="js-projects/tooltip/" target="_blank">View app</a></li>				
 					<li>Bar Animate Percentage: <a href="js-projects/bars/" target="_blank">View app</a></li>				
-					<li>Infinite Carousel: <a href="js-projects/carousel-infinite/" target="_blank">View app</a></li>
 					<li>Custom Form Dropdown <br>(Turn JavaScript off for accessible version): <a href="js-projects/customDropdown/" target="_blank">View app</a></li>
-					<li>Timeline pin animation: (First time using objects) <a href="js-projects/2012timeline/" target="_blank">View app</a></li>
 					<li>Image grower: <a href="js-projects/imageGrower/" target="_blank">View app</a></li>
 					<li>Animated Tweeting: <a href="js-projects/animateTweeting/" target="_blank">View app</a></li>
 					<li>Album filter: <a href="js-projects/filter/" target="_blank">View app</a></li>
@@ -27,13 +27,13 @@ $ogtags = 'http://www.rohanchambers.net/img/global/fb_logo.png'
 					<li>Slide carousel, horizontal &amp; vertical: <a href="js-projects/slidecarousel.0.2/" target="_blank">View app</a></li>
 					<li>Rohan carousel auto play <a href="js-projects/carousel-auto/" target="_blank">View app</a></li>
 					<li>Rohan carousel + lightbox <a href="js-projects/carousel-lightbox/" target="_blank">View app</a></li>
-					<li>Rohan carousel <a href="js-projects/carousel/" target="_blank">View app</a></li>
+					<li>Rohan carousel <a href="js-projects/carousel/" target="_blank">View app</a> (August 10, 2011)</li>
 					<li>Lightbox <a href="js-projects/lightbox/" target="_blank">View app</a></li>
 					<li>Fixed flat sidebar <a href="js-projects/fixedFloatSidebar/" target="_blank">View app</a></li>				
 					<li>Preload image <a href="js-projects/preloadImage/" target="_blank">View app</a></li>
 					<li>Image fade in <a href="js-projects/imageFadeIn/" target="_blank">View app</a></li>
 					<li>Button switch <a href="js-projects/buttonSwitch/" target="_blank">View app</a></li>
-					<li>Float content clear (xhtml, css, cross browser) <a href="js-projects/floatContentClear/" target="_blank">View app</a></li>
+					<li>Float content clear (xhtml, css, cross browser) <a href="js-projects/floatContentClear/" target="_blank">View app</a> (July 10, 2010)</li>
 				</ul>
 			</section><!-- /section -->						
 		</div><!-- /content -->

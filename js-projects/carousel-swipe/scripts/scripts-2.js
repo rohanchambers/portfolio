73c923fc1466d2a3dcc3e23ba7856a5d73a32ed0
Rohan Chambers <rohanchambers888@yahoo.com>
2014-06-10 23:57:04 +0100

    update swipe carousel need to add disable when gets to end of carousel

--- js-projects/carousel-swipe/scripts/scripts-2.js
+++ js-projects/carousel-swipe/scripts/scripts-2.js
@@ -1,60 +1,55 @@
 $(document).ready(function(){
+
 	var carousel = $('#carousel');
 	var slides = $('#carousel li');
 	var slideWidth = $('#carousel li').width();
 	var totalSlides = $('#carousel li').length;
 	var totalSlidesWidth = (totalSlides * slideWidth);
 	var speed = 500;
+	var currentPos = 1;	
 		
 	//On Load
 	$('#carousel').css('width', totalSlidesWidth + 'px');
-	
-	$('#carousel li:first').before($('#carousel li:last')); 		
-	
+
 	//Buttons NXT
-	$('.btn-nxt').bind('click', function(){
+	$("#carousel").swipe({
+	  swipe:function(phase, event, direction, distance, duration, fingerCount){
+	    $('h2').text("You swiped " + direction + " for " + distance + "px" );
+	  },	  
+	  threshold:20,
 
-		$('#carousel:not(:animated)').animate({'left' : '-=' + slideWidth + 'px'}, speed, function(){
-		
-			$('#carousel li:last').after($('#carousel li:first'));
-			$('#carousel').css({'left' : '-800px'});  		
-			
-		});
-		
+	  swipeLeft:function(phase, event, direction, distance, duration, fingerCount){
+	  	currentPos++
+	  	alert(currentPos)	  
+	  	$('#carousel:not(:animated)').animate({'left' : '-=' + slideWidth + 'px'}, speed);
+
+		if(currentPos == 1){
+			console.log('first');
+		};
+	  },
+
+	  swipeRight:function(event, direction, distance, duration, fingerCount){
+	  	currentPos--
+	  	alert(currentPos)
+	  	$('#carousel:not(:animated)').animate({'left' : '+=' + slideWidth + 'px'}, speed);
+
+		if(currentPos == 4){
+			 alert('Last')
+		};
+	  }
+
 	});
-	
-	//Buttons PREV
-	$('.btn-prev').bind('click', function(){		
-				
-		$('#carousel:not(:animated)').animate({'left' : '+=' + slideWidth + 'px'}, speed, function(){
 
-			$('#carousel li:first').before($('#carousel li:last'));
-			$('#carousel').css({'left' : '-800px'});  		
-		
-		});
-		
-	});	
 
-	//Auto Play
-	var intervalId = 0;
-	var timer = 2000;
-	
-	function autoPlay(){
 
-		$('.btn-nxt').trigger('click');
-				
-	};
-	
-	intervalId = setInterval (autoPlay, timer);	
 
-	$('#carousel, .btn-nxt, .btn-prev').hover( function(){
+	// $('.btn-nxt').bind('click', function(){
+	// 	$('#carousel:not(:animated)').animate({'left' : '-=' + slideWidth + 'px'}, speed);
+	// });
 	
-		 clearTimeout(intervalId);
-	
-	}, function(){
-	
-		intervalId = setInterval (autoPlay, timer);	
-		
-	});
-			
+	// //Buttons PREV
+	// $('.btn-prev').bind('click', function(){					
+	// 	$('#carousel:not(:animated)').animate({'left' : '+=' + slideWidth + 'px'}, speed);		
+	// });	
+
 });
